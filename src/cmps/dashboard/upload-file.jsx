46ef3e364eab4e66ab1fae68d67aae2d134806c6
Matan Damary <matandamary10@gmail.com>
2022-08-12 11:21:49 +0300

    changing filereader function

--- src/cmps/dashboard/upload-file.jsx
+++ src/cmps/dashboard/upload-file.jsx
@@ -1,7 +1,7 @@
 import { dataService } from "../../services/data.service"
 import { fileService } from "../../services/file.service"
-import { utilService } from "../../services/util.service"
 import { BsUpload } from 'react-icons/bs'
+
 
 export const UploadFile = (props) => {
     const { setExcelData, setLoading } = props
@@ -13,16 +13,16 @@ export const UploadFile = (props) => {
             if (fileService.getFileTypes().includes(file.type)) {
                 let data = await fileService.getFileData(file)
                 data.shift()
-                let newData = {}
                 data = data.map(obj => {
-                    newData._id = utilService.makeId()
-                    newData.date = obj.__EMPTY_1 * 86400 - 172800 - (31556926 * 70)
-                    newData.code = obj.__EMPTY_2
-                    newData.description = obj.__EMPTY_3
-                    newData.income = obj.__EMPTY_5
-                    newData.expense = obj.__EMPTY_6
-                    newData.balance = obj.__EMPTY_8
-                    return { ...newData }
+                    const objKey = ['date', 'code', 'description', 'reference', 'income', 'expense', 'date-value', 'balance']
+                    const objKeys = {}
+                    const distructedObj = Object.values(obj)
+                    objKey.forEach((obj, idx) => {
+                        objKeys[obj] = distructedObj[idx]
+                    })
+                    objKeys.date = objKeys.date * 86400 - 172800 - (31556926 * 70)
+                    objKeys['date-value'] = objKeys['date-value'] * 86400 - 172800 - (31556926 * 70)
+                    return { ...objKeys }
                 })
                 dataService.save(data)
                 setExcelData(data)
